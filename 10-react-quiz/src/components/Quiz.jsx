884b0ad3f9269e2aafbf30721cf55fc0f5dc0848
Tarak Barjadiya <tarakbarjadiya@gmail.com>
2024-07-01 18:37:46 +0530

    Added Question Timer Functionality

--- 10-react-quiz/src/components/Quiz.jsx
+++ 10-react-quiz/src/components/Quiz.jsx
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import QUESTIONS from "../assets/questions.js";
 import quizCompleteLogo from "../assets/quiz-complete.png";
+import QustionTimer from "./QuestionTimer.jsx";
+
+let QUESTION_TIMER = 10000;
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -8,11 +11,13 @@ export default function Quiz() {
 
   const quizIsOver = activeQuesionIndex === QUESTIONS.length;
 
-  function handleClickAnswer(selectedAnswer) {
+  const handleClickAnswer = useCallback(function handleClickAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
     });
-  }
+  }, []);
+
+  const handleSkipAnswer = useCallback(() => handleClickAnswer(null), [handleClickAnswer]);
   if (quizIsOver) {
     return (
       <div id="summary">
@@ -26,17 +31,15 @@ export default function Quiz() {
   shuffledAnswers.sort(() => {
     return Math.random() - 0.5;
   });
-
   return (
     <div id="quiz">
       <div id="question">
+        <QustionTimer key={activeQuesionIndex} timeout={QUESTION_TIMER} onTimeout={handleSkipAnswer} />
         <h2>{QUESTIONS[activeQuesionIndex].text}</h2>
         <ul id="answers">
           {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
-              <button onClick={() => handleClickAnswer(answer)}>
-                {answer}
-              </button>
+              <button onClick={() => handleClickAnswer(answer)}>{answer}</button>
             </li>
           ))}
         </ul>
