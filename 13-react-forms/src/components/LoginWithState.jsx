80789db9cbca85ac2525cb70fb00da4b1d96ea39
Tarak Barjadiya <tarakbarjadiya@gmail.com>
2024-07-26 12:04:10 +0530

    Login --- Signup +++

--- 13-react-forms/src/components/LoginWithState.jsx
+++ 13-react-forms/src/components/LoginWithState.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export default function Login() {
+export default function LoginWithState() {
   // const [enteredEmail, setEnteredEmail] = useState('');
   // const [enteredPassword, setEnteredPassword] = useState('');
 
