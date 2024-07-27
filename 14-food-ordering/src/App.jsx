3ab89e195114d59556accb40a3458fcad055c93d
Tarak Barjadiya <tarakbarjadiya@gmail.com>
2024-07-27 11:54:15 +0530

    Eslint configured

--- 14-food-ordering/src/App.jsx
+++ 14-food-ordering/src/App.jsx
@@ -1,11 +1,11 @@
 function App() {
-  return (
-    <>
-      <h1>You got this 💪</h1>
-      <p>Stuck? Not sure how to proceed?</p>
-      <p>Don't worry - we've all been there. Let's build it together!</p>
-    </>
-  );
+    return (
+        <>
+            <h1>You got this 💪</h1>
+            <p>Stuck? Not sure how to proceed?</p>
+            <p>Don&apos;t worry - we&apos;ve all been there. Let&apos;s build it together!</p>
+        </>
+    );
 }
 
 export default App;
