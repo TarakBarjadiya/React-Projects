6cf1943caf438ba83fde6c8b12b6e272b4d72236
Tarak Barjadiya <tarakbarjadiya@gmail.com>
2025-01-02 14:29:56 +0530

    Cart Complete

--- 14-food-ordering/src/components/Header.jsx
+++ 14-food-ordering/src/components/Header.jsx
@@ -1,7 +1,14 @@
+import { useContext } from 'react';
 import LOGO from '../assets/logo.jpg';
 import Button from './UI/Button.jsx';
+import CartContext from '../store/CartContext.jsx';
 
 export default function Header() {
+  const cartCtx = useContext(CartContext);
+
+  const totalCartItems = cartCtx.items.reduce((totalNumberofItems, item) => {
+    return totalNumberofItems + item.quantity;
+  }, 0);
   return (
     <>
       <header id="main-header">
@@ -10,7 +17,7 @@ export default function Header() {
           <h1>React Food</h1>
         </div>
         <nav>
-          <Button textOnly>Cart (0)</Button>
+          <Button textOnly>Cart ({totalCartItems})</Button>
         </nav>
       </header>
     </>
