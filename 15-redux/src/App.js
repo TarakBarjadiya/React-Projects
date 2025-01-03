c47bf72c011e7b773e942d0e7882d7903b7ff906
Tarak Barjadiya <tarakbarjadiya@gmail.com>
2025-01-03 18:51:37 +0530

    Redux Basic Complete

--- 15-redux/src/App.js
+++ 15-redux/src/App.js
@@ -1,9 +1,19 @@
+import { useSelector } from 'react-redux';
+
+import Header from './components/Header';
+import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
 import Counter from './components/Counter';
 
-
 function App() {
+  const isLoggedIn = useSelector((state) => state.auth.isAuthenticated);
+
   return (
-    <Counter />
+    <>
+      <Header />
+      {!isLoggedIn ? <Auth /> : <UserProfile />}
+      <Counter />
+    </>
   );
 }
 
