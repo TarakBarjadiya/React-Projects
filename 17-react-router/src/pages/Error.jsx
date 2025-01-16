8fb2ab2412dfd1caafe19bad97b0b7215e818f34
Tarak Barjadiya <tarakbarjadiya@gmail.com>
2025-01-16 13:50:59 +0530

    Router Basics

---
+++ 17-react-router/src/pages/Error.jsx
@@ -0,0 +1,13 @@
+import MainNavigation from '../components/MainNavigation';
+
+export default function Error() {
+  return (
+    <>
+      <MainNavigation />
+      <main>
+        <h1>404 - An Error occurred!!</h1>
+        <p>Could not find the page!!</p>
+      </main>
+    </>
+  );
+}
--- 17-react-router/src/pages/Home.jsx
+++ 17-react-router/src/pages/Home.jsx
@@ -1,3 +1,18 @@
-export default function Home(){
-    return <h1>Home Page</h1>
+import { Link, useNavigate } from 'react-router-dom';
+
+export default function Home() {
+  const navigate = useNavigate();
+  
+  function handleNavigate(){
+    navigate('products')
+  }
+  return (
+    <>
+      <h1>Home Page</h1>
+      <p>
+        Go to <Link to="products">the link of products</Link>
+      </p>
+      <button onClick={handleNavigate}>Navigate</button>
+    </>
+  );
 }
